d79c4e40749d4522b21d2780f17a27684859c60d
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-11-03 01:26:33 -0500

    [allergy-registry-processor]: add ipfs client

--- rest-api/clients/common/Constants.js
+++ rest-api/clients/common/Constants.js
@@ -11,6 +11,14 @@ class Constants {
     static ACTION_CREATE = 'CREATE';
     static ACTION_UPDATE = 'UPDATE';
     static ACTION_DELETE = 'DELETE';
+
+    //IPFS INFURA
+    static IPFS_INFURA_URL = 'https://ipfs.infura.io:5001/api/v0';
+    static IPFS_INFURA_PROTOCOL = 'https';
+    static IPFS_INFURA_HOST = 'ipfs.infura.io';
+    static IPFS_INFURA_PORT = '5001';
+    static IPFS_INFURA_API_KEY = '2XEndWqaClJmMVzOkdFiwJLXvHC';
+    static IPFS_INFURA_API_KEY_SECRET = 'de5a46a82e7cee86e579d6403aac259a';
 }
 
 module.exports.Constants = Constants;
---
+++ rest-api/clients/common/InfuraIPFSClient.js
@@ -0,0 +1,84 @@
+const { Constants } = require('./Constants');
+const axios = require('axios');
+
+class InfuraIPFSClient {
+    constructor() {
+        this.instance = axios.create({
+            baseURL: Constants.IPFS_INFURA_URL,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            },
+            auth: {
+                username: Constants.IPFS_INFURA_API_KEY,
+                password: Constants.IPFS_INFURA_API_KEY_SECRET,
+            },
+        });
+    }
+
+    async add(data) {
+        var response = {
+            hash: '',
+            error: true
+        };
+
+        var fd = new FormData();
+        fd.append('file', JSON.stringify(data));
+        await this.instance.post('/add', fd)
+            .then((res) => {
+                response.hash = res.data.Hash;
+                response.error = false;
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+
+        return response;
+    }
+
+    async cat(hash) {
+        var response = {
+            data: '',
+            error: true
+        };
+
+        await this.instance.post('/cat', {}, {
+                params: {
+                    arg: hash
+                }
+            })
+            .then((res) => {
+                console.log("ipfs cat | res: ", res.data);
+                response.data = res.data;
+                response.error = false;
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+
+        return response;
+    }
+
+    async rm(hash) {
+        var response = {
+            data: '',
+            error: true
+        };
+        await this.instance.post('/pin/rm', {}, {
+                params: {
+                    arg: hash
+                }
+            })
+            .then((res) => {
+                response.data = res.data;
+                response.error = false;
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+        
+        return response;
+    }
+}
+
+module.exports.InfuraIPFSClient = InfuraIPFSClient;
