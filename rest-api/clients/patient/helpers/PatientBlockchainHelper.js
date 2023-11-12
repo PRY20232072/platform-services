0dd97ae8b704c42e23534e34a9d1ff98bb517205
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-11-11 23:55:24 -0500

    [patietn-processor]: delete coomment

--- rest-api/clients/patient/helpers/PatientBlockchainHelper.js
+++ rest-api/clients/patient/helpers/PatientBlockchainHelper.js
@@ -1,5 +1,3 @@
-// const { TextEncoder } = require('text-encoding/lib/encoding');
-// const { InfuraIPFSClient } = require('../../common/InfuraIPFSClient');
 const { CommonBlockchainHelper } = require('../../common/helpers/CommonBlockchainHelper');
 const { Constants } = require('../../common/Constants');
 
@@ -10,46 +8,7 @@ class PatientBlockchainHelper extends CommonBlockchainHelper {
             Constants.PATIENT_REGISTRY_TP_CODE,
             Constants.PATIENT_REGISTRY_TP_VERSION
         );
-
-        // this.infuraIPFSClient = new InfuraIPFSClient();
     }
-
-    // async wrap_and_send(identifier, payload, address) {
-    //     var enc = new TextEncoder('utf8');
-
-    //     //TODO: encrypt payload
-
-    //     var new_payload = {};
-    //     if (payload.action !== Constants.ACTION_DELETE) {
-    //         var response = await this.infuraIPFSClient.add(payload);
-    
-    //         if (response.error) {
-    //             return response;
-    //         }
-    //         // console.log("save ipfs hash: ", response.hash);
-    
-    //         //send object to blockchain like a string
-    //         var new_payload = JSON.stringify({
-    //             patient_id: identifier,
-    //             ipfs_hash: response.hash,
-    //             action: payload.action
-    //         });
-    //     }
-    //     else {
-    //         new_payload = payload;
-    //     }
-        
-    //     var payloadBytes = enc.encode(new_payload);
-
-    //     var txnHeaderBytes = this.make_txn_header_bytes(payloadBytes, address);
-    //     var txnBytes = this.make_txn_bytes(txnHeaderBytes, payloadBytes);
-
-    //     response = await this.saveDataInBlockchain('/batches', txnBytes);
-
-    //     //TODO: validate if response has info
-
-    //     return response;
-    // }
 }
 
 module.exports.PatientBlockchainHelper = PatientBlockchainHelper;
